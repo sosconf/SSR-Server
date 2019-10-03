0a1a7c5021838922b17d464e894fae93441295bc
100101010 <36503136+100101010@users.noreply.github.com>
2019-10-03 11:51:38 +0800

    add shell script to check IP and resources

---
+++ check.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ ! -f "./output.logs" ];then
+touch output.logs
+fi
+
+# check the server port
+serverIP="152.44.45.234"
+port=2333
+IPstatus=$(nc -z -w 1 $serverIP $port && echo ok || echo no)
+if [ "$IPstatus" == "ok" ];then
+    echo `date`:"server port is normal" | tee -a output.logs
+else
+    echo `date`:"server port is abnormal" | tee -a output.logs
+fi
+
+# check third-party resources
+resource1="raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssrmu.sh"
+urlstatus=$(curl -s -m 4 -IL $resource1|grep 200)
+if [ "$urlstatus" == "" ];then
+    echo `date`:"$resource1 is OFF" | tee -a output.logs
+else
+    echo `date`:"$resource1 is OK" | tee -a output.logs
+fi
+
+resource2="github.com/teddysun/across/raw/master/bbr.sh"
+urlstatus=$(curl -s -m 2 -IL $resource2|grep 200)
+if [ "$urlstatus" == "" ];then
+    echo `date`:"$resource2 is OFF" | tee -a output.logs
+else
+    echo `date`:"$resource2 is OK" | tee -a output.logs
+fi
